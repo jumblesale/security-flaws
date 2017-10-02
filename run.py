ee8ef9d2edeb5d22cb7fa29797df7da70fd176f9
Rob Sollars <rsollars@ea.com>
2017-10-02 16:18:05 +0100

    removed creating schema on run.py

--- run.py
+++ run.py
@@ -6,8 +6,8 @@ sys.path.append(os.path.join(os.path.dirname(__file__), "security_flaws"))
 from security_flaws.app import app
 import security_flaws.db as db
 
-with app.app_context():
-    db.create_schema(db.get_db())
-    db.insert_fixtures(db.get_db())
+# with app.app_context():
+#     db.create_schema(db.get_db())
+#     db.insert_fixtures(db.get_db())
 
 app.run()
